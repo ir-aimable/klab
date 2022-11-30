eac1101e62cda5a72a7eead38e50ecc76cc73f25
Aimable Iradukunda <53192761+ir-aimable@users.noreply.github.com>
2022-11-30 16:52:34 +0200

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,28 @@
+function price(){
+
+const item = [ {name: 'Bike', price:100}, {name: 'TV', price:200}, {name: 'Album', price:10}, {name: 'Book', price:5}, {name: 'Phone', price:500}, {name: 'Computer', price:1000}, ];
+let arr = [];
+let arr2 = [];
+// returning the cheap and expensive products
+for(const prop in item){
+  if(item[prop].price < 100){
+    arr = item[prop];
+    return arr;
+  }else if(item[prop].price > 500){
+    arr2 = item[prop];
+    return arr2;
+  }
+}
+
+// returning the sum of prices
+const Values = Object.values(item);
+const sum = values.reduce((accumulator, value) => {
+  return accumulator + value;
+}, 0);
+
+console.log(sum);
+
+console.log(arr);
+  
+}
+price();
